257ba4618ca20d62a5cef1efd930cbcaed00a783
Akash Mahmud <mahmudakash177@gmail.com>
2022-12-15 18:17:30 +0600

    deployment automation

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,14 @@ import Image from 'next/image'
 
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline text-blue-600">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Deployment automation with unit testing and component testing implemented
+      </h1>
+    </>
+  );
 }
